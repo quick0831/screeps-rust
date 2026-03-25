ecdf199b581209cd058d2bd2b5037d1bd9916d4c
Quick Chang <quick0831@gmail.com>
2026-03-26 00:21:03 +0800

    add role to creep

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,27 @@
-use screeps::{Part, find, game};
+use serde::Deserialize;
+use serde::Serialize;
+use serde_wasm_bindgen::to_value;
+
+use screeps::Part;
+use screeps::SpawnOptions;
+use screeps::find;
+use screeps::game;
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+enum Roles {
+    Harvester,
+}
+
+#[derive(Serialize)]
+struct CreepMem {
+    role: Roles,
+}
 
 #[unsafe(export_name = "loop")]
 pub extern "C" fn func_loop() {
+    let time = game::time();
+
     let creeps = game::creeps();
     let spawn = game::spawns().values().next().unwrap();
 
@@ -12,6 +32,11 @@ pub extern "C" fn func_loop() {
 
     if creeps.keys().count() == 0 {
         let body = vec![Part::Move, Part::Work, Part::Carry];
-        let _ = spawn.spawn_creep(&body, "Test");
+        let name = format!("Harvester{time}");
+        let mem = CreepMem {
+            role: Roles::Harvester,
+        };
+        let option = SpawnOptions::new().memory(to_value(&mem).unwrap());
+        let _ = spawn.spawn_creep_with_options(&body, &name, &option);
     }
 }
