3b977329ab5a210164d0e1e4823167f310a22416
Quick Chang <quick0831@gmail.com>
2026-05-13 04:50:39 +0800

    improve harvester logic

--- src/harvester.rs
+++ src/harvester.rs
@@ -1,5 +1,7 @@
 use screeps::Creep;
+use screeps::ResourceType;
 use screeps::SharedCreepProperties;
+use screeps::StructureType;
 use screeps::action_error_codes::HarvestErrorCode;
 use screeps::action_error_codes::TransferErrorCode;
 use screeps::find;
@@ -10,16 +12,31 @@ use crate::SharedData;
 #[derive(Debug, Default, Serialize, Deserialize)]
 pub struct HarvesterMemory {}
 
-pub fn run(creep: &Creep, _memory: &mut HarvesterMemory, d: &SharedData) {
+pub fn run(creep: &Creep, _memory: &mut HarvesterMemory, _d: &SharedData) {
     if creep.store().get_free_capacity(None) > 0 {
         let sources = creep.room().unwrap().find(find::SOURCES, None);
         if let Err(HarvestErrorCode::NotInRange) = creep.harvest(&sources[0]) {
             let _ = creep.move_to(&sources[0]);
         }
     } else {
-        let err = creep.transfer(&d.spawn, screeps::ResourceType::Energy, None);
-        if let Err(TransferErrorCode::NotInRange) = err {
-            let _ = creep.move_to(d.spawn.clone());
+        let structures = creep.room().unwrap().find(find::MY_STRUCTURES, None);
+        let mut targets = structures.into_iter().filter(|s| {
+            matches!(
+                s.structure_type(),
+                StructureType::Extension | StructureType::Spawn | StructureType::Tower
+            ) && s
+                .as_has_store()
+                .map(|s| s.store().get_free_capacity(Some(ResourceType::Energy)))
+                .unwrap_or(0)
+                > 0
+        });
+        if let Some(target) = targets.next()
+            && let Some(transferable) = target.as_transferable()
+        {
+            let err = creep.transfer(transferable, screeps::ResourceType::Energy, None);
+            if let Err(TransferErrorCode::NotInRange) = err {
+                let _ = creep.move_to(target.clone());
+            }
         }
     }
 }
