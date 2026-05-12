9c392b3b36cdc99fe0c29bd6f048f187da49ee0d
Quick Chang <quick0831@gmail.com>
2026-05-13 00:53:28 +0800

    implement builder

--- src/builder.rs
+++ src/builder.rs
@@ -1,4 +1,8 @@
-use screeps::Creep;
+use screeps::{
+    Creep, ResourceType,
+    action_error_codes::{BuildErrorCode, HarvestErrorCode},
+    find,
+};
 use serde::{Deserialize, Serialize};
 
 use crate::SharedData;
@@ -8,6 +12,27 @@ pub struct BuilderMemory {
     building: bool,
 }
 
-pub fn run(_creep: &Creep, _memory: &mut BuilderMemory, _d: &SharedData) {
-    // todo
+pub fn run(creep: &Creep, memory: &mut BuilderMemory, _d: &SharedData) {
+    if memory.building && creep.store().get(ResourceType::Energy).unwrap_or(0) == 0 {
+        memory.building = false;
+        let _ = creep.say("🔄 harvest", false);
+    }
+    if !memory.building && creep.store().get_free_capacity(None) == 0 {
+        memory.building = true;
+        let _ = creep.say("🚧 build", false);
+    }
+
+    if memory.building {
+        let construction_sites = creep.room().unwrap().find(find::CONSTRUCTION_SITES, None);
+        if let Some(construction_site) = construction_sites.first()
+            && let Err(BuildErrorCode::NotInRange) = creep.build(construction_site)
+        {
+            let _ = creep.move_to(construction_site);
+        }
+    } else {
+        let sources = creep.room().unwrap().find(find::SOURCES, None);
+        if let Err(HarvestErrorCode::NotInRange) = creep.harvest(&sources[0]) {
+            let _ = creep.move_to(&sources[0]);
+        }
+    }
 }
