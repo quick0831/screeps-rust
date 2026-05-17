1316269f81a5fb0d0811f314f5896b00b62df8ae
Quick Chang <quick0831@gmail.com>
2026-05-18 05:38:32 +0800

    only allow deposit to containers when it's nearby

--- src/roles/harvester.rs
+++ src/roles/harvester.rs
@@ -11,6 +11,7 @@ use screeps::find;
 use serde::{Deserialize, Serialize};
 
 use crate::SharedData;
+use crate::utils::diagonal_distance;
 use crate::utils::sort_unstable_by_distance;
 
 #[derive(Debug, Default, Serialize, Deserialize)]
@@ -50,6 +51,7 @@ pub fn run(creep: &Creep, memory: &mut HarvesterMemory, d: &SharedData) {
             .find(find::STRUCTURES, None)
             .into_iter()
             .filter(|s| matches!(s.structure_type(), StructureType::Container))
+            .filter(|s| diagonal_distance(creep.pos(), s.pos()) <= 2)
             .filter(|s| {
                 s.as_has_store()
                     .map(|s| s.store().get_free_capacity(Some(ResourceType::Energy)))
--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,13 @@
+use std::cmp::max;
+
 use screeps::HasPosition;
 use screeps::Position;
+
+pub fn diagonal_distance(a: Position, b: Position) -> u8 {
+    let (ax, ay) = a.coords();
+    let (bx, by) = b.coords();
+    max(ax.abs_diff(bx), ay.abs_diff(by))
+}
 
 pub fn sort_unstable_by_distance<T: HasPosition>(center: Position, mut sites: Vec<T>) -> Vec<T> {
     let (cx, cy) = center.coords_signed();
