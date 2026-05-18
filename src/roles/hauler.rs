90f473fa66ad4d955150ec0bdf59db9f9e7134f6
Quick Chang <quick0831@gmail.com>
2026-05-19 00:13:09 +0800

    cache fetching location for haulers

--- src/roles/hauler.rs
+++ src/roles/hauler.rs
@@ -1,5 +1,8 @@
 use screeps::Creep;
+use screeps::ObjectId;
 use screeps::ResourceType;
+use screeps::StructureContainer;
+use screeps::StructureObject;
 use screeps::StructureType;
 use screeps::action_error_codes::TransferErrorCode;
 use screeps::action_error_codes::WithdrawErrorCode;
@@ -13,6 +16,7 @@ use crate::utils::sort_unstable_by_distance;
 #[derive(Debug, Default, Serialize, Deserialize)]
 #[serde(default)]
 pub struct HaulerMemory {
+    target: Option<ObjectId<StructureContainer>>,
     carrying: bool,
 }
 
@@ -22,6 +26,21 @@ pub fn run(creep: &Creep, memory: &mut HaulerMemory, _d: &SharedData) {
     }
     if creep.store().get(ResourceType::Energy).unwrap_or(0) == 0 {
         memory.carrying = false;
+
+        let structures = creep.room().unwrap().find(find::STRUCTURES, None);
+        let mut targets = structures
+            .into_iter()
+            .filter_map(|s| {
+                if let StructureObject::StructureContainer(c) = s {
+                    let energy = c.store().get(ResourceType::Energy).unwrap_or(0);
+                    Some((c, energy))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+        targets.sort_unstable_by_key(|(_, energy)| *energy);
+        memory.target = targets.last().map(|(c, _)| c.id());
     }
 
     if memory.carrying {
@@ -50,26 +69,12 @@ pub fn run(creep: &Creep, memory: &mut HaulerMemory, _d: &SharedData) {
                 let _ = creep.move_to(target.clone());
             }
         }
-    } else {
-        let structures = creep.room().unwrap().find(find::STRUCTURES, None);
-        let mut targets = structures
-            .into_iter()
-            .filter(|s| matches!(s.structure_type(), StructureType::Container))
-            .filter(|s| {
-                s.as_has_store()
-                    .and_then(|s| s.store().get(ResourceType::Energy))
-                    .unwrap_or(0)
-                    > 0
-            })
-            .collect::<Vec<_>>();
-        targets = sort_unstable_by_distance(creep.pos(), targets);
-        if let Some(target) = targets.first()
-            && let Some(withdrawable) = target.as_withdrawable()
-        {
-            let err = creep.withdraw(withdrawable, ResourceType::Energy, None);
-            if let Err(WithdrawErrorCode::NotInRange) = err {
-                let _ = creep.move_to(target.clone());
-            }
+    } else if let Some(target) = memory.target
+        && let Some(target) = target.resolve()
+    {
+        let err = creep.withdraw(&target, ResourceType::Energy, None);
+        if let Err(WithdrawErrorCode::NotInRange) = err {
+            let _ = creep.move_to(target.clone());
         }
     }
 }
