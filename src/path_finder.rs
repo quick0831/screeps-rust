980dc8b40d76cb36b58e66dbf87ee7f8b7ca698d
Quick Chang <quick0831@gmail.com>
2026-05-26 01:17:07 +0800

    add structure info to cost matrix

--- src/path_finder.rs
+++ src/path_finder.rs
@@ -1,7 +1,9 @@
 use screeps::CostMatrix;
 use screeps::Creep;
 use screeps::Position;
+use screeps::RoomName;
 use screeps::action_error_codes::CreepMoveByPathErrorCode;
+use screeps::find;
 use screeps::game;
 use screeps::pathfinder::MultiRoomCostResult;
 use screeps::pathfinder::SearchOptions;
@@ -10,25 +12,32 @@ use screeps::prelude::*;
 
 const COST_UNWALKABLE: u8 = 255;
 
+// TODO: cache the result when queried on the same tick
+fn get_costmatrix(room_name: RoomName) -> MultiRoomCostResult {
+    let Some(room) = game::rooms().get(room_name) else {
+        return MultiRoomCostResult::Default;
+    };
+
+    let cost_matrix = CostMatrix::new();
+
+    let creep_pos = room.find(find::CREEPS, None).into_iter().map(|c| c.pos());
+    let structure_pos = room
+        .find(find::MY_STRUCTURES, None)
+        .into_iter()
+        .map(|s| s.pos());
+    for pos in creep_pos.chain(structure_pos) {
+        let (x, y) = pos.coords();
+        cost_matrix.set(x, y, COST_UNWALKABLE);
+    }
+    MultiRoomCostResult::CostMatrix(cost_matrix)
+}
+
 pub fn path_away_from(
     creep: &Creep,
     target: Position,
     range: u32,
 ) -> Result<(), CreepMoveByPathErrorCode> {
-    // TODO: cache the result when queried on the same tick
-    let callback = |room| {
-        let cost_matrix = CostMatrix::new();
-        for c in game::creeps().values() {
-            let pos = c.pos();
-            if pos.room_name() == room {
-                let (x, y) = pos.coords();
-                cost_matrix.set(x, y, COST_UNWALKABLE);
-            }
-        }
-        MultiRoomCostResult::CostMatrix(cost_matrix)
-    };
-
-    let options = SearchOptions::new(callback)
+    let options = SearchOptions::new(get_costmatrix)
         .flee(true)
         .max_rooms(1)
         .max_ops(100);
